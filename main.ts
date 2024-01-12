a15b1497930a89ff42dfdbca897c4585c94b987f
FERNANDA EDUARDO MATTOS <114959869+FernandaEduardoMattos@users.noreply.github.com>
2024-01-12 13:25:44 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Mão = 0
+input.onGesture(Gesture.Shake, function () {
+    Mão = randint(1, 3)
+    if (Mão == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Mão == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
